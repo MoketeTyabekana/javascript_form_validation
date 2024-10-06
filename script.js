885f111e7a7abf755060f48e89b38c7ababa0f48
Mokete <emtyabekana@gmail.com>
2024-10-06 21:08:20 +0200

    Initialised new fields and assigned the input value of the user to these fields and also used trim to remove any white spaces on the inputs.

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ form.addEventListener('submit',e=>{
 
 function validateInputs(){
 
+    const usernameValue=username.value.trim();
+    const emailValue=email.value.trim();
+    const passwordVlue=password.value.trim();
+    const password2Value=password2.value.trim();
     
     
 }
